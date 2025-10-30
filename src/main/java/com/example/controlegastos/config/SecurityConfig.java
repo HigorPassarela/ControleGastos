32ffdd71a98b4043b585b4678239139b52ee074c
HigorPassarela <hhpassarela@gmail.com>
2025-10-30 15:27:40 -0300

    Criacao SecurityConfig, alteracao properties e notacao na entity Usuario.java

---
+++ src/main/java/com/example/controlegastos/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.controlegastos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
+    }
+}
--- src/main/java/com/example/controlegastos/model/usuarios/Usuario.java
+++ src/main/java/com/example/controlegastos/model/usuarios/Usuario.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -32,6 +33,7 @@ public class Usuario {
     @NotBlank(message = "Senha é obrigatória")
     private String senha;
 
+    @CreationTimestamp
     @Column(nullable = false)
     private LocalDateTime dataCadastro;
 
