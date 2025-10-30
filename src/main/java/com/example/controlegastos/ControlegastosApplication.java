ee475fce8888d6686e1a3dc394a6bb754cf6105d
HigorPassarela <hhpassarela@gmail.com>
2025-10-29 22:25:17 -0300

    primeiro commit.

---
+++ src/main/java/com/example/controlegastos/ControlegastosApplication.java
@@ -0,0 +1,13 @@
+package com.example.controlegastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlegastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlegastosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/controlegastos/ControlegastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controlegastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlegastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
